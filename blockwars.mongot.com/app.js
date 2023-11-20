405af8108d29899286347315be09ac06f2f5bec0
royal <roynilsson87@gmail.com>
2023-11-20 19:23:54 +0100

    update packages

--- blockwars.mongot.com/app.js
+++ blockwars.mongot.com/app.js
@@ -1,10 +1,9 @@
 'use strict';
 const http = require('http');
-const csrf = require('csurf');
 const express = require('express');
+const sessions = require('client-sessions');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const sessions = require('client-sessions');
 const flash = require('express-flash');
 const middleware = require('../middleware');
 const config = require('../config.json');
@@ -14,7 +13,7 @@ const app = express();
  * Connect to mongoDB
  */
 mongoose.connect(config.PROD_DB)
-.catch(err => console.log(err));
+  .catch(err => console.log(err));
 //mongoose.connect(config.PROD_DB);
 
 /**
@@ -40,7 +39,7 @@ let server = http.createServer(app).listen(port);
  * Makes it possible to get static files from view
  */
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 /**
@@ -49,38 +48,26 @@ app.use(bodyParser.json());
 //app.use(require('./serviceworker'));
 
 /**
- * Create new user session for auth
- */
-module.exports.createUserSession = function(req, res, user){
-    let setSessionInfo = {
-        username: user.username,
-        email: user.email
-    };
-    req.session.user = setSessionInfo;
-    req.user = setSessionInfo;
-    res.locals.user = setSessionInfo;
-};
-
-/**
  * Session rules
  */
-app.use(sessions({
-    cookieName: 'session',
-    secret: '345gyu345g3785g785g578g563gf25673f56734f56723',// Random string as secret
-    httpOnly: true, // not letting javascript access cookies
-    // ephemeral: true, // delete cookies when the browser is closed
-    //secure: true, // only use cookies over https
-    duration: 3600 * 1000 * 24 * 365, // cookie expires after one year
-    activeDuration: 3600 * 1000 * 24 * 30 // cookie expire after 30 days if user is not active
-    // cookie: {
-    //     maxAge: 3600 * 1000 * 24 * 365,
-    //     httpOnly: true, 
-    //     domain: '.mongot.com'
-    // }
-}));
-
-app.use(csrf());
-
+//app.set('trust proxy', true);
+app.use(
+  sessions({
+      cookieName: "session",
+      secret: "345gyu345g3785g785g578g563gf25673f56734f56723", // Random string as secret
+      httpOnly: true, // not letting javascript access cookies
+      // ephemeral: true, // delete cookies when the browser is closed
+      //secure: true, // only use cookies over https
+      duration: 3600 * 1000 * 24 * 365, // cookie expires after one year
+      activeDuration: 3600 * 1000 * 24 * 30, // cookie expire after 30 days if user is not active
+      cookie: {
+          sameSite: 'strict',
+          //     maxAge: 3600 * 1000 * 24 * 365,
+          //     httpOnly: true,
+          //     domain: 'mongot.com'
+      }
+  })
+);
 /**
  * Middleware, Refresh session on page loads
  */
@@ -92,15 +79,15 @@ app.use(middleware.cookieAuth);
 app.use(require('./routes/main'));
 
 // Handle 404
-app.use(function(req, res) {
-    res.status(400);
-    res.render('404');
+app.use(function (req, res) {
+  res.status(400);
+  res.render('404');
 });
 
 // Handle 500
-app.use(function(error, req, res, next) {
-    res.status(500);
-    res.send('500: Internal Server Error - ' + error);
+app.use(function (error, req, res, next) {
+  res.status(500);
+  res.send('500: Internal Server Error - ' + error);
 });
 
 require('./game/server.js').initialize(server);
